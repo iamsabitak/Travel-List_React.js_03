95c0dcd9903b9c1cfd08c53daace14f4a82a010b
sabita <sabitakhadka091@gmail.com>
2023-10-18 15:46:32 +0545

    Add import statements for components: Form, Logo, PackingList, Stats and item

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import "./index.css";
 
 import React, { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "Charger", quantity: 1, packed: true },
-//   { id: 3, description: "Mobile", quantity: 1, packed: true },
-// ];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
